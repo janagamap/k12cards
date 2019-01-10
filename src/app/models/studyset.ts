79a0228049ae40db0457d0a949a70ce695370442
Prashanth <janagama.prashanth@gmail.com>
2019-01-09 21:58:17 -0500

    Styling, passing data to child component.

--- src/app/models/studyset.ts
+++ src/app/models/studyset.ts
@@ -2,5 +2,6 @@ export interface Studyset {
     id: number;
     title: String;
     description: String;
+    items: number;
     image?: String;
 }
--- src/app/services/studyset/studysetlist.ts
+++ src/app/services/studyset/studysetlist.ts
@@ -3,11 +3,13 @@ import {Studyset} from '../../models/studyset';
 export let studysets: Array<Studyset> = [{
     id: 1,
     title: 'The Inside Story',
-    description: 'Matt Armstrong Quiz',
+    description: 'Matt Armstrong Quiz, Matt Armstrong Quizl Matt Armstrong Quiz Matt Armstrong Quiz',
+    items: 4
 },
 {
     id: 2,
     title: 'LoTR Quiz',
-    description: 'How well do you know your rings?'
+    description: 'How well do you know your rings? How well do you know your rings? How well do you know your rings? ',
+    items: 5
 }
 ];
